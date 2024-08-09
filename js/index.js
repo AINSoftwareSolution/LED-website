8efe85583a86e7175dccdfb138ecf29a3470c4f6
Shahid shah <114140444+Shahid7424@users.noreply.github.com>
2024-08-09 13:23:49 +0530

    add section

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Initialize AOS (Animate On Scroll) library
+  AOS.init({
+      duration: 2000, // Default duration for animations
+      once: true // Whether animation should happen only once
+  });
+});
